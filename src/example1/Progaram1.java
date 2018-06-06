351b2a690334f732038c97c113490d91444a2f45
elgieldhose <39951748+elgieldhose@users.noreply.github.com>
2018-06-06 14:18:42 +0530

    Update Progaram1.java
    change the program

--- src/example1/Progaram1.java
+++ src/example1/Progaram1.java
@@ -3,7 +3,7 @@ package example1;
 public class Progaram1 {
 
   public static void main(String[] args) {
-   System.out.println("First Program");
+   System.out.println("First Program...................................");
 
   }
 
